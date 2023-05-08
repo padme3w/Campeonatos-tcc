5ab031a19074f322f7d4e8772e49371fc44629e0
padme3w <amandaoliveira080605@gmail.com>
2023-05-08 17:01:52 -0300

    o js das 3 pontuações

--- js/guardDecrement.js
+++ js/guardDecrement.js
@@ -1,5 +1,7 @@
 const guardDecrementButton1 = document.querySelector('#guardDecrementButton1');
 const guardIncrementButton1 = document.querySelector('#guardIncrementButton1');
+const guardDecrementButton2 = document.querySelector('#guardDecrementButton2');
+const guardIncrementButton2 = document.querySelector('#guardIncrementButton2');
 
 function guardDecrement1(){
     console.log('decrement');
@@ -9,9 +11,22 @@ function guardDecrement1(){
       player1TotalPoints.innerHTML = "<p>" + (parseInt(player1TotalPoints.innerText) - 3) + "</p>";
     }
 }
-function guardIncrement2(){
+function guardIncrement1(){
   console.log('increment');
   player1TotalPoints.innerHTML = "<p>" + (parseInt(player1TotalPoints.innerText) + 3) + "</p>";
+}
+
+function guardDecrement2(){
+  console.log('decrement');
+  if(parseInt(player2TotalPoints.innerText) <= 0) {
+    player2TotalPoints.innerHTML = "<p>" + 0 + "</p>";
+  } else {
+    player2TotalPoints.innerHTML = "<p>" + (parseInt(player2TotalPoints.innerText) - 3) + "</p>";
+  }
+}
+function guardIncrement2(){
+console.log('increment');
+player2TotalPoints.innerHTML = "<p>" + (parseInt(player2TotalPoints.innerText) + 3) + "</p>";
 }
 
 guardDecrementButton1.addEventListener('click', () => {
@@ -19,6 +34,14 @@ guardDecrementButton1.addEventListener('click', () => {
 });
   
 guardIncrementButton1.addEventListener('click',() => {
-    guardIncrement2();
+    guardIncrement1();
+});
+
+guardDecrementButton2.addEventListener('click', () => {
+  guardDecrement2();
+});
+
+guardIncrementButton2.addEventListener('click',() => {
+  guardIncrement2();
 });
   
---
+++ js/overthrowFunctions.js
@@ -0,0 +1,47 @@
+const overthrowDecrementButton1 = document.querySelector('#overthrowDecrementButton1');
+const overthrowIncrementButton1 = document.querySelector('#overthrowIncrementButton1');
+const overthrowDecrementButton2 = document.querySelector('#overthrowDecrementButton2');
+const overthrowIncrementButton2 = document.querySelector('#overthrowIncrementButton2');
+
+function overthrowDecrement1(){
+    console.log('decrement');
+    if(parseInt(player1TotalPoints.innerText) <= 0) {
+      player1TotalPoints.innerHTML = "<p>" + 0 + "</p>";
+    } else {
+      player1TotalPoints.innerHTML = "<p>" + (parseInt(player1TotalPoints.innerText) - 2) + "</p>";
+    }
+}
+function overthrowIncrement1(){
+  console.log('increment');
+  player1TotalPoints.innerHTML = "<p>" + (parseInt(player1TotalPoints.innerText) + 2) + "</p>";
+}
+
+function overthrowDecrement2(){
+  console.log('decrement');
+  if(parseInt(player2TotalPoints.innerText) <= 0) {
+    player2TotalPoints.innerHTML = "<p>" + 0 + "</p>";
+  } else {
+    player2TotalPoints.innerHTML = "<p>" + (parseInt(player2TotalPoints.innerText) - 2) + "</p>";
+  }
+}
+function overthrowIncrement2(){
+console.log('increment');
+player2TotalPoints.innerHTML = "<p>" + (parseInt(player2TotalPoints.innerText) + 2) + "</p>";
+}
+
+overthrowDecrementButton1.addEventListener('click', () => {
+  overthrowDecrement1();
+});
+  
+overthrowIncrementButton1.addEventListener('click',() => {
+  overthrowIncrement1();
+});
+
+overthrowDecrementButton2.addEventListener('click', () => {
+  overthrowDecrement2();
+});
+
+overthrowIncrementButton2.addEventListener('click',() => {
+  overthrowIncrement2();
+});
+  
