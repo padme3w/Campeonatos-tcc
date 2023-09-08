8087ec5bd61f962d86279bd0490ec93ef0a28e01
padme3w <amandaoliveira080605@gmail.com>
2023-09-08 15:44:35 -0300

    esboço do ranking

---
+++ ranking.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+
+    <link rel="stylesheet" href="css/ranking.css">
+</head>
+<body>
+    <main>
+        <h1>Equipes vencedoras</h1>
+
+        <div class="1"> 
+            <img src="imgs/trofeu.png" alt="troféu dourado">
+            1° Lugar:
+            <div class="equipe"></div>
+        </div>
+
+        <div class="2"> 
+            <img src="imgs/trofeu.png" alt="troféu dourado">
+            2° Lugar:
+            <div class="equipe"></div>
+        </div>
+
+        <div class="3"> 
+            <img src="imgs/trofeu.png" alt="troféu dourado">
+            3° Lugar:
+            <div class="equipe"></div>
+        </div>
+
+        <div class="4"> 
+            <img src="imgs/trofeu.png" alt="troféu dourado">
+            4° Lugar:
+            <div class="equipe"></div>
+        </div>
+
+        <div class="5"> 
+            <img src="imgs/trofeu.png" alt="troféu dourado">
+            5° Lugar:
+            <div class="equipe"></div>
+        </div>
+    </main>
+</body>
+</html>
